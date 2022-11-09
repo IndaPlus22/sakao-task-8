3342cc1fb2000bd0650da078a032b42abc183158
toshi <toshi.sakao.r@gmail.com>
2022-11-10 07:23:32 +0900

    i am dumb so i want to keep this version

--- findprimes.asm
+++ findprimes.asm
@@ -1,67 +1,57 @@
-##################################################################
-#
-#   MIPS assembly code example
-#   - Hello World as application 
-#
-#   Author: Tobias Hansson <tohans@kth.se>
-#
-#   Created: 2020-10-23
-#
-#   See: MARS Syscall Sheet (https://courses.missouristate.edu/KenVollmar/mars/Help/SyscallHelp.html)
-#   See: MIPS Documentation (https://ecs-network.serv.pacific.edu/ecpe-170/tutorials/mips-instruction-set)
-#
-##################################################################
-
-### Data Declaration Section ###
-
 .data
 
-dumb_msg: .asciiz "number must be between 1 <n < 1001\n"
+primes:		.space  1000               						# reserves a block of 2 (sizeof(short)) * 168(number of primes up to 1000) bytes in application memory
+dumb_msg:	.asciiz "number must be between 1 <n < 1001\n"
 
 ### Executable Code Section ###
 
 .text
 
 main:
-    # get input
+    # get input n
     li      $v0, 5                  # reads integer from input
     syscall
     
     #check that number is in range
     li	    $t1, 1001
-    slt     $t0, $v0,$t1	         # t0 = v0 < 1001, t0 is 0 or 1
-    beq     $v0, $zero, dumb_people    # if input was 0 goto dumb
-    beq     $t0, $zero, dumb_people  # if input was bigger than 1000 goto dumb
-    
-    
-    
-    li      $v0, 1                  # magic to print a number
-    add     $a0, $t0, $zero         # print number
-    syscall
+    slt     $t0, $v0,$t1	           # t0 = v0 < 1001, t0 is bool
+    beq     $t0, $zero, dumb_people    # if input was 0 goto dumb
+    beq     $t1, $zero, dumb_people   # if input was bigger than 1000 goto dumb
 
-    # # print HW
-    # li      $v0, 4                  # magic code to print string
-    # la      $a0, HW                 # load address of string HW into $a0
-    # syscall                         # HW now printed
+    #li		$v0, 1                  # print if number was ok
+    #add     $a0, $t0, $zero         
+    #syscall
+    
+    #make prime number array
+    la      $t0, primes				# $s1 is now pointer to primes[0]
+    li		$t1, 0					# counter
+    li		$t2, 2					# first number in primes array
+
+# main loop
+while_loop:
+	
+	sb		$t2, ($t0)				# primes[$t1] = $t2
+	srl		$t3, $t2, 8				# shift number by 1 byte
+	sb		$t3, 1($t0)
+	
+	#incrementing shit
+	addi	$t2, $t2, 1				    # $t2++ next number to check
+	addi	$t1, $t1, 1					# $t1++ counter
+	addi	$t0, $t0, 2					# go to next element in primes (short)
+	
+	#checking if end
+	bne		$t1, $v0, while_loop	# if counter < input, loop again
+
+# loop end 
 
     # exit program
     li $v0, 10                      # set system call code to "terminate program"
     syscall                         # terminate program
     
 dumb_people:
-    # li       $t0, .asciiz "number must be between 1 <n < 1001\n"
     li       $v0, 4
     la       $a0, dumb_msg
     syscall
 
     li $v0, 10                      # set system call code to "terminate program"
     syscall                         # terminate program
-
-
-##################################################################
-#
-#   NOTE:
-#       Applications assembled and executed by MARS, or applications which terminate at EOF,
-#       don't need the termination call. This termination call is therefore unnessecary.
-#
-##################################################################
