5c0b5c1b364d513ed3305fcf119d2a49cac2a7d9
toshi <toshi.sakao.r@gmail.com>
2022-11-10 09:00:34 +0900

    almost there, problem is it is running all 1000 times when not needed every time lol

--- findprimes.asm
+++ findprimes.asm
@@ -25,25 +25,56 @@ main:
     #make prime number array
     la      $t0, primes				# $s1 is now pointer to primes[0]
     li		$t1, 0					# counter
-    li		$t2, 2					# first number in primes array
+    li		$t2, 1					# first number in primes array
 
-# main loop
-while_loop:
+# initialize loop
+init_loop:
 	
 	sb		$t2, ($t0)				# primes[$t1] = $t2
-	srl		$t3, $t2, 8				# shift number by 1 byte
-	sb		$t3, 1($t0)
 	
 	#incrementing shit
-	addi	$t2, $t2, 1				    # $t2++ next number to check
 	addi	$t1, $t1, 1					# $t1++ counter
-	addi	$t0, $t0, 2					# go to next element in primes (short)
+	addi	$t0, $t0, 1					# go to next element in primes
 	
 	#checking if end
-	bne		$t1, $v0, while_loop	# if counter < input, loop again
-
+	bne		$t1, $v0, init_loop	# if counter < input, loop again
 # loop end 
 
+		
+	la      $t0, primes				# $s1 is now pointer to primes[0]
+	li		$t1, 1					# counter in for main_loop
+	li		$t2, 0
+	li		$s1, 32					# max sqrt of n
+	li		$s0, 2					# false
+#main loop of determining
+main_loop:
+	add		$t7, $t0, $zero			 # saves t0
+	
+	#incrementing shit
+	addi	$t1, $t1, 1					# $t1++ counter of main_loop
+	addi	$t0, $t0, 1					# go to next element
+	
+	lb		$t6, ($t7)
+	beq		$t6, $s0, main_loop		 # if primes[i] == false, loop again from main_loop
+	
+
+	mul		$t4, $t1, $t1			# first number to check
+	la		$t5, primes
+	add		$t5, $t5, $t4			# 
+	subi	$t5, $t5, 1
+	
+sec_loop:
+	sb		$s0, ($t5)
+	
+	add		$t5, $t5, $t1			# incrementing pointer by t1(counter of main_loop)
+	add		$t4, $t4, $t1			# incrementing counter by t1
+	ble		$t4, $v0, sec_loop		# if i <= n		
+# sec_loop end
+
+	bne		$t1, $s1, main_loop		# if i != max, loop again !!!!!!!!!
+#main_loop end
+
+	
     # exit program
     li $v0, 10                      # set system call code to "terminate program"
     syscall                         # terminate program
